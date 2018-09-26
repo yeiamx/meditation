28a55cdf1f8704d89e372c75883a3f028cb7cc1d
chusiyuan <chusiyuan96@gmail.com>
2018-09-26 21:52:22 +0800

    login_ui

--- front_end/Meditation/app/src/main/java/com/mrxia/meditation/MyApplication.java
+++ front_end/Meditation/app/src/main/java/com/mrxia/meditation/MyApplication.java
@@ -5,4 +5,5 @@ import android.app.Application;
 public class MyApplication extends Application {
     //public static final String urlStarter = "http://39.108.226.195:8080/meditation";
     public static final String urlStarter = "http://192.168.199.121:8080/meditation";
+    public static boolean isLogin = false;
 }
---
+++ front_end/Meditation/app/src/main/java/com/mrxia/meditation/layout/Login.java
@@ -0,0 +1,14 @@
+package com.mrxia.meditation.layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mrxia.meditation.R;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
